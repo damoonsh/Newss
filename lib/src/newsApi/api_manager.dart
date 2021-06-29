f69eb8d78f5963261ef15ef993fb0e46879f2fca
Damoon Shah <damoon.shah3@gmail.com>
2021-06-28 23:33:54 -0400

    Working on the search process

--- lib/src/newsApi/api_manager.dart
+++ lib/src/newsApi/api_manager.dart
@@ -4,15 +4,33 @@ import 'package:http/http.dart' as http;
 
 import 'news_model.dart';
 
-// Url api to get the articles
-final Uri url = Uri.parse(
-    "https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=84f2bbf63c7d4afe8419396d901fb246");
+// Some constants
+
+final String api_key = "84f2bbf63c7d4afe8419396d901fb246";
+final String base_url = "https://newsapi.org/v2";
+
+// defualt Url api to get the articles
+final Uri topHeadlineUrl = Uri.parse(
+    "$base_url/top-headlines?country=us&pageSize=100&apiKey=$api_key");
+
+// Client module for making api calls
+final http.Client client = http.Client();
+
+Future<NewsModel> searchArticles(String keyword, [String category = ""]) async {
+  Uri searchUrl = Uri.parse(category == ""
+      ? "$base_url/everything?q=$keyword"
+      : "$base_url/everything?q=$keyword&category=$category");
+
+  print(keyword);
+
+  http.Response response = await client.get(searchUrl);
+
+  return NewsModel.fromJson(jsonDecode(response.body));
+}
 
 // Fetching articles
 Future<NewsModel> fetchArticles() async {
-  final http.Client client = http.Client();
-
-  http.Response response = await client.get(url);
+  http.Response response = await client.get(topHeadlineUrl);
 
   return NewsModel.fromJson(jsonDecode(response.body));
 }
